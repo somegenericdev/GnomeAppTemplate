834d94d315cb4db434a40e48174d7df6cad901d4
somegenericdev <95114021+somegenericdev@users.noreply.github.com>
2025-08-17 20:05:04 +0200

    gt4 example

---
+++ Gtk4App.App/Program.cs
@@ -0,0 +1,16 @@
+﻿var application = Gtk.Application.New("org.kashif-code-samples.gtk4app.app", Gio.ApplicationFlags.FlagsNone);
+application.OnActivate += (sender, args) =>
+{
+    var label = Gtk.Label.New("Hello World!");
+    label.SetMarginTop(12);
+    label.SetMarginBottom(12);
+    label.SetMarginStart(12);
+    label.SetMarginEnd(12);
+
+    var window = Gtk.ApplicationWindow.New((Gtk.Application)sender);
+    window.Title = "GTK Gtk4App App";
+    window.SetDefaultSize(300, 300);
+    window.Child = label;
+    window.Show();
+};
+return application.RunWithSynchronizationContext(null);
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # GnomeAppTemplate
 
+#GTK3
+
 Debian dependencies:
 
 ```
 sudo apt install libgtk-3-dev
+```
+
+#GTK4
+
+```
+sudo apt install libgtk-4-dev
+sudo apt install blueprint-compiler
+dotnet new install TenderOwl.GirCoreTemplate.CSharp
 ```
