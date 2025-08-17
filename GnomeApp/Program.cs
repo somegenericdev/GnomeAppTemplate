f1d9f82a40914dc030f781a5c1df977c3d59a6bf
somegenericdev <95114021+somegenericdev@users.noreply.github.com>
2025-08-17 04:44:07 +0200

    added CSS example

--- GnomeApp/Program.cs
+++ GnomeApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Gdk;
 using GnomeApp;
 using Gtk;
+using Window = Gtk.Window;
 
 Application.Init();
 
@@ -10,7 +12,6 @@ window.DeleteEvent += (o, e) => Application.Quit();
 
 //tab bar
 var notebook = new Notebook();
-
 //tab 1
 var tab1Label = new Label("Hello World. Welcome to your new app.");
 notebook.AppendPage(tab1Label, new Label("Home"));
@@ -19,6 +20,7 @@ notebook.AppendPage(tab1Label, new Label("Home"));
 int counter = 0;
 var counterBox = new VBox();
 var button = new Button("Click Me");
+button.Name = "my-button";
 var counterLabel = new Label($"Counter: {counter}");
 
 button.Clicked += (sender, e) =>
@@ -36,12 +38,41 @@ notebook.AppendPage(counterBox, new Label("Counter"));
 //tab 3
 var weatherBox = new VBox();
 
-var grid = BuildGrid(new List<WeatherForecastDto>(){new WeatherForecastDto(DateTime.Now, 30, "Hot"), new WeatherForecastDto(DateTime.Now.AddDays(-10), 10, "Cold")});
+var grid = BuildGrid(new List<WeatherForecastDto>()
+    { new WeatherForecastDto(DateTime.Now, 30, "Hot"), new WeatherForecastDto(DateTime.Now.AddDays(-10), 10, "Cold") });
 weatherBox.PackStart(grid, true, true, 0);
 notebook.AppendPage(weatherBox, new Label("Weather"));
 
 
 window.Add(notebook);
+
+
+// Create CSS provider and load CSS
+var cssProvider = new CssProvider();
+
+string css = @"
+            #my-button {
+                color: blue;
+                font-weight: bold;
+                border-radius: 50px;
+                padding: 10px;
+            }
+
+            #my-button:hover {
+                background-color: #2980b9;
+            }
+        ";
+
+cssProvider.LoadFromData(css);
+
+// Add CSS provider to the default screen
+StyleContext.AddProviderForScreen(
+    Screen.Default,
+    cssProvider,
+    StyleProviderPriority.Application
+);
+
+
 window.ShowAll();
 Application.Run();
 
@@ -54,6 +85,7 @@ TreeView BuildGrid(List<WeatherForecastDto> weatherForecasts)
     {
         store.AppendValues(forecast.Date.ToString("dd/MM/yyyy"), forecast.Temp, forecast.Summary);
     }
+
     treeView.Model = store;
 
     var nameColumn = new TreeViewColumn { Title = "Date" };
@@ -67,8 +99,8 @@ TreeView BuildGrid(List<WeatherForecastDto> weatherForecasts)
     ageColumn.PackStart(ageCell, true);
     ageColumn.AddAttribute(ageCell, "text", 1);
     treeView.AppendColumn(ageColumn);
-    
-    
+
+
     var summaryColumn = new TreeViewColumn { Title = "Summary" };
     var summaryCell = new CellRendererText();
     summaryColumn.PackStart(summaryCell, true);
