8744bb4f0a12cb9e13c7304af623744b8875931f
somegenericdev <95114021+somegenericdev@users.noreply.github.com>
2025-08-17 03:51:23 +0200

    init

---
+++ GnomeApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Gtk;
+
+
+Application.Init();
+
+var window = new Window("Hello GNOME with C#");
+window.DeleteEvent += (o, e) => Application.Quit();
+window.SetDefaultSize(400, 200);
+
+var label = new Label("Welcome to GNOME app in C#!");
+window.Add(label);
+
+window.ShowAll();
+Application.Run();
